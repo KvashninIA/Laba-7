f3089e989c0fbdbc7eb3cbaa015052f3b2099882
Ivan Kvashnin <147997865+KvashninIA@users.noreply.github.com>
2024-01-15 20:46:50 +0500

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,48 @@
+package Lr7;
+
+//Напишите программу, в которой есть суперкласс с приватным текстовым полем,
+//конструктором с текстовым параметром и где переопределен метод toString (). На основе
+//суперкласса путем наследования создается подкласс. У него появляется еще одно
+//приватное текстовое ноле. Также подкласс должен иметь версии конструктора с одним и двумя текстовыми аргументами, а еще в нем должен быть переопределен метод toString ().
+//В обоих классах метод toString () переопределяется так, что он возвращает строку с названием класса и значение текстового поля или текстовых полей.
+class SuperClass {
+    private String text;
+    public SuperClass(String text) {
+        this.text = text;
+    }
+    @Override
+    public String toString() {
+        return "SuperClass: " + text;
+    }
+}
+class SubClass extends SuperClass {
+    private String additionalText;
+    public SubClass(String text) {
+        super(text);
+    }
+    public SubClass(String text, String additionalText) {
+        super(text);
+        this.additionalText = additionalText;
+    }
+    @Override
+    public String toString() {
+        if (additionalText != null) {
+            return "SubClass: " + getText() + ", " + additionalText;
+        } else {
+            return "SubClass: " + getText();
+        }
+    }
+    private String getText() {
+        return super.toString().substring(12); // Remove "SuperClass: " from the string
+    }
+}
+public class Example1 {
+    public static void main(String[] args) {
+        SuperClass superObject = new SuperClass("Hello");
+        System.out.println(superObject);
+        SubClass subObject1 = new SubClass("Hi");
+        System.out.println(subObject1);
+        SubClass subObject2 = new SubClass("Hey", "There");
+        System.out.println(subObject2);
+    }
+}
---
+++ Example5.java
@@ -0,0 +1,66 @@
+package Lr7;
+
+//Напишите программу, в которой есть суперкласс с защищенным текстовым полем,
+//конструктор с текстовым параметром и метод, при вызове которого в консольном окне
+//отображается название класса и значение поля. На основе суперкласса создаются два
+//подкласса (оба на основе одного и того же суперкласса). В одном из классов появляется
+//защищенное целочисленное поле, там есть конструктор с двумя параметрами и
+//переопределен метод для отображения значений полей объекта и названия класса. Во
+//втором подклассе появляется защищенное символьное поле, конструктор с двумя
+//параметрами и переопределен метод, отображающий в консоли название класса и значения
+//полей. В главном методе создайте объекты каждого из классов. Проверьте работу метода,
+//отображающего значения полей объектов, для каждого из объектов. Вызовите этот же
+//метод через объектную переменную суперкласса, которая ссылается на объект
+//производного класса
+class SuperClass5 {
+    protected String textField;
+
+    public SuperClass5(String textField) {
+        this.textField = textField;
+    }
+
+    public void displayInfo() {
+        System.out.println("Class: SuperClass, Text Field: " + textField);
+    }
+}
+
+class FirstSubClass extends SuperClass5 {
+    protected int intValue;
+
+    public FirstSubClass(String textField, int intValue) {
+        super(textField);
+        this.intValue = intValue;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Class: FirstSubClass, Text Field: " + textField + ", Int Field: " + intValue);
+    }
+}
+
+class SecondSubClass extends SuperClass5 {
+    protected char charValue;
+
+    public SecondSubClass(String textField, char charValue) {
+        super(textField);
+        this.charValue = charValue;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Class: SecondSubClass, Text Field: " + textField + ", Char Field: " + charValue);
+    }
+}
+
+public class Example5 {
+    public static void main(String[] args) {
+        FirstSubClass firstObj = new FirstSubClass("Hello", 10);
+        SecondSubClass secondObj = new SecondSubClass("World", 'A');
+
+        firstObj.displayInfo();
+        secondObj.displayInfo();
+
+        SuperClass5 superClassRef = firstObj;
+        superClassRef.displayInfo();
+    }
+}
